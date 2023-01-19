9d10d4053e7ef75554d238843d9cf1131ad745a6
Christian <chriscar.sam@gmail.com>
2023-01-18 22:06:04 -0500

    Se modificó la lógica del mensaje de información

--- js/script.js
+++ js/script.js
@@ -15,14 +15,22 @@ function encriptar(){
             document.getElementById("mensaje").style.background = "white";
             document.getElementById("mensaje").innerHTML = cifrar;  
             document.getElementById("informacion").innerHTML = "";      
-            document.getElementById("copiar").style.display = "block";                 
+            document.getElementById("copiar").style.display = "block";  
+            document.getElementById("warning").style.opacity = "0";   
+            document.getElementById("check").style.opacity = "60%";
+            document.getElementById("informacion").innerHTML = "Encriptar";             
         } else {
             document.getElementById("text-area").value = "";         
-            document.getElementById("informacion").innerHTML = "Solo letras minúsculas y sin acentos";           
+            
+            document.getElementById("informacion").innerHTML = "Solo letras minúsculas y sin acentos";   
+            document.getElementById("warning").style.opacity = "100%"     
+            document.getElementById("check").style.opacity = "0";   
         }
 
-    } else {
-        document.getElementById("informacion").innerHTML = "! Ingrese un texto";  
+    } else {       
+        document.getElementById("informacion").innerHTML = "Ingrese el texto";  
+        document.getElementById("warning").style.opacity = "40%";
+        document.getElementById("check").style.opacity = "0";
     }
 }
 
@@ -33,10 +41,13 @@ function desencriptar(){
     if(texto != "" ){   // validar que el campo no este vacio    
         cifrar = texto.replaceAll(llaves[0], letras[0]).replaceAll(llaves[1], letras[1]).replaceAll(llaves[2], letras[2]).replaceAll(llaves[3], letras[3]).replaceAll(llaves[4], letras[4]);
 
-        document.getElementById("mensaje").innerHTML = cifrar;
+        document.getElementById("informacion").innerHTML = "Descencriptar";  
+        document.getElementById("mensaje").innerHTML = cifrar;        
     
-    } else {
-        document.getElementById("informacion").innerHTML = "! Ingrese un texto";
+    } else {        
+        document.getElementById("informacion").innerHTML = "Ingrese el texto";
+        document.getElementById("warning").style.opacity = "60%"
+        document.getElementById("check").style.opacity = "0";
     }
 }
 
@@ -50,10 +61,11 @@ function copiar(){
         texto.setSelectionRange(0, 99999);
     
         navigator.clipboard.writeText(texto.value);
-        document.getElementById("text-area").value = ""; //Vaciar texto-area luego de copiar el texto
-    
-        alert("Texto copiado: " + texto.value);
-    } else {
-        alert("No existe texto a copiar.");
-    }
+        document.getElementById("text-area").value = ""; //Vaciar texto-area luego de copiar el texto         
+        // document.getElementById("mensaje").style.backgroundImage = `url(../img/muñeco2.png)`;  
+       
+        document.getElementById("informacion").innerHTML = "Texto copiado: " + texto.value;
+        document.getElementById("check").style.opacity = "60%";
+        document.getElementById("warning").style.opacity = "0"    
+    } 
 }
